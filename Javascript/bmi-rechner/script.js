af9ed188ce4e379213fd2e9f457661731ae64ce2
TomDBaer <thomasbaehren@gmail.com>
2024-03-01 09:17:02 +0100

    projekt bmi rechner

---
+++ Javascript/bmi-rechner/script.js
@@ -0,0 +1,12 @@
+
+gewicht = parseFloat(prompt("Wie viel wiegst du in kg")).replace(",", ".")
+groesse = parseInt(prompt("Wie groß bist du in cm")) / 100
+
+// Alternativ
+// Hier ist die eingabe in Meter. Ein Switch check wäre noch vom vorteil ob cm oder meter
+// groesse = parseFloat(prompt("Wie groß bist du in meter")).replace(",", ".")
+
+// Es gibt schönere Rechenarten als x*x. ist aber verständlich
+bmi = (gewicht / (groesse * groesse)).toFixed(2) 
+
+alert(`Du hast bei einer Größe von ${groesse}cm und einem Gewicht von ${gewicht}kg einen BMI von: ${bmi}`)
