d99a95559681cb47da9851f4a8e08ea6888a9ee6
TomDBaer <thomasbaehren@gmail.com>
2024-03-04 14:58:15 +0100

    changing const variable and adding some design

--- Javascript/text-laenge/script.js
+++ Javascript/text-laenge/script.js
@@ -1,17 +1,18 @@
-const max_length = 35;
+const MAX_LENGTH = 35;
 
 let text_eingabe = document.querySelector(".eingabe").textContent;
 let text_ausgabe = document.querySelector(".ausgabe")
-let num_ausgabe = document.querySelector(".numba")
+let num_ausgabe = document.querySelector(".info")
 
 let num_text = text_eingabe.length;
 
-let new_text = text_eingabe.slice(0,max_length)
+let new_text = text_eingabe.slice(0, MAX_LENGTH)
 
-let diff_length = num_text - max_length
+let diff_length = num_text - MAX_LENGTH
 
 text_ausgabe.insertAdjacentText('afterbegin', new_text)
 
-num_ausgabe.insertAdjacentText('afterbegin', `Die Differenz ist ${diff_length}`)
-
-
+num_ausgabe.insertAdjacentText('afterbegin', `Der Text ist länger als die mögliche länge von ${MAX_LENGTH} Zeichen. 
+    Er wurde aus diesem grund gekürzt.
+    Die differenz beträgt ${diff_length} Zeichen
+`)
