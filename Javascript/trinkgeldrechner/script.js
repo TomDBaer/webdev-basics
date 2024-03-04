575b245955d6cf96e639e720958d7bfd4e79f1a6
TomDBaer <thomasbaehren@gmail.com>
2024-03-04 15:22:22 +0100

    adding error handling

--- Javascript/trinkgeldrechner/script.js
+++ Javascript/trinkgeldrechner/script.js
@@ -1,53 +1,33 @@
-// Project Tip Calculator
-
-// TODO 1. Erstellen Sie eine Begrüßung für Ihr Programm.
-
-// TODO 2. Fragen Sie den Benutzenden wie hoch die komplette Rechnung ist.
-
-// TODO 3. Fragen Sie den Benutzenden wie viel Trinkgeld (in %) gegeben werden soll.
-
-// TODO 4. Fragen Sie anschließend auf wie viele Personen die Rechnung aufgeteilt werden soll.
-
-// TODO 5. Das Programm soll nun den zu zahlenden Betrag für jede einzelne Person inklusive des Trinkgeldes berechnen.
-
-// Beispiel
-// Wenn die Rechnung beispielsweise einen Betrag von 150€ hatte, diese auf 5
-// Personen aufgeteilt werden soll und dabei ein Trinkgeld von 10 % gegeben wird,
-// dann sollte jede Person (150.00 / 5) * 1,1 zahlen
-
-// Runden Sie das Ergebnis auf zwei dezimale Kommastellen
-
-// (Math.round(num * 100) / 100).toFixed(2);
+let ausgabe = document.querySelector(".ausgabe");
 
 has_error = false;
-error = "";
+error_message = "";
 
 alert(`Schönen guten Tag, Sie wollen also Trinkgeld geben?`);
 let betrag_gesamt = Number(prompt("Wie hoch ist der Betrag den Sie zu bezahlen haben?"));
 let prozent_trinkgeld = Number(prompt("Wie viel Prozent an Trinkgeld wollen Sie geben?"));
 let personen = Number(prompt("Wie viele Personen sind in Ihrer Gruppe?"));
 
-//console.log(typeof betrag_gesamt);
-//console.log(typeof personen);
 
-// Funktioniert so nicht. Ich kann auch einen char in eine int setzen. Stört dann bei der Berechnung
-// Am besten bei der Berechnung selber Überprüfen ob die Rechnung eine NaN ausgibt
-//if (typeof betrag_gesamt != "number" || typeof prozent_trinkgeld != "number" || typeof personen != "number"){
-//    has_error = !has_error;
-//    error = "Sie haben einen Wert eingegeben der keine Zahl ist\nBitte starten Sie die Rechnung neu";
-//    alert(error)
-//    throw new Error();
-//}
+let betrag_pro_person = betrag_gesamt / personen;
+let einzelbetrag_mit_trinkgeld = Math.round(betrag_pro_person * (prozent_trinkgeld / 100 + 1)).toFixed(2);
+let endbetrag_mit_trinkgeld = einzelbetrag_mit_trinkgeld * personen;
 
-betrag_pro_person = betrag_gesamt / personen;
-einzelbetrag_mit_trinkgeld = Math.round( betrag_pro_person * (prozent_trinkgeld / 100 + 1)).toFixed(2);
-endbetrag_mit_trinkgeld = einzelbetrag_mit_trinkgeld * personen;
+if (Number.isNaN(betrag_pro_person) || Number.isNaN(einzelbetrag_mit_trinkgeld) || Number.isNaN(endbetrag_mit_trinkgeld)) {
+    has_error = true;
+    error_message = "Eine der Eingaben war keine Nummer!"
+}
 
-console.log(`Testlog`);
+console.log(`Testlog:`);
 console.log(`Es sind ${personen} in Ihrer Gruppe.
 \nJeder muss bei ${prozent_trinkgeld}% Trinkgeld einen Betrag von ${einzelbetrag_mit_trinkgeld}€ Bezahlen.
 \nZusammen macht das ${endbetrag_mit_trinkgeld}€`);
 
-alert(`Es sind ${personen} in Ihrer Gruppe.
-\nJeder muss bei ${prozent_trinkgeld}% Trinkgeld einen Betrag von ${einzelbetrag_mit_trinkgeld}€ Bezahlen.
-\nZusammen macht das ${endbetrag_mit_trinkgeld}€`);
+if (has_error) {
+    ausgabe.innerHTML = `${error_message}`
+} else {
+    // Ausgabe nachdem ich alle Prompt durch habe
+    ausgabe.innerHTML = `Es sind ${personen} in Ihrer Gruppe.<br />
+    Jeder muss bei ${prozent_trinkgeld}% Trinkgeld einen Betrag von ${einzelbetrag_mit_trinkgeld}€ Bezahlen.<br />
+    Zusammen macht das ${endbetrag_mit_trinkgeld}€`;
+}
