a0e29834f0354284a13da0e7ba2232549d8309a8
TomDBaer <thomasbaehren@gmail.com>
2024-03-04 16:08:34 +0100

    ausgabe HTML hinzu gefügt

--- Javascript/bmi-rechner-v2/script.js
+++ Javascript/bmi-rechner-v2/script.js
@@ -1,4 +1,16 @@
-// replace error
+// INFO
+// Auswerung nach
+// Untergewicht < 18,5
+// Normalgewicht 18,5 - 24,9
+// Übergewicht: > 25,0
+// Präadipositas 25,0 - 29,9
+// moderate Adipositas 30,0 - 34,9
+// starke Adipositas 35,0 - 39,9
+// extreme Adipositas > 40,0
+
+// error gibt es nicht
+let auswertung = document.querySelector(".auswertung");
+let kommentar_auswertung = document.querySelector(".kommentar");
 let gewicht = parseFloat(prompt("Wie viel wiegst du in kg").replace(",", "."));
 let groesse = parseInt(prompt("Wie groß bist du in cm")) / 100;
 let kommentar = "";
@@ -10,14 +22,7 @@ let kommentar = "";
 // Es gibt schönere Rechenarten als x*x. ist aber verständlich
 let bmi = (gewicht / (groesse * groesse)).toFixed(2)
 
-// Auswerung nach
-// Untergewicht < 18,5
-// Normalgewicht 18,5 - 24,9
-// Übergewicht: > 25,0
-// Präadipositas 25,0 - 29,9
-// moderate Adipositas 30,0 - 34,9
-// starke Adipositas 35,0 - 39,9
-// extreme Adipositas > 40,0
+
 
 // If Statement
 if (bmi < 18.5) {
@@ -25,12 +30,22 @@ if (bmi < 18.5) {
 }
 else if (bmi >= 18.5 && bmi <= 24.9) {
     kommentar = "Sieht gut aus, Normalgewicht"
-} else {
-    kommentar = "Ooof"
+}
+else if (bmi >= 25.0 && bmi <= 29.9) {
+    kommentar = "Präadipositas"
+}
+else if (bmi >= 30.0 && bmi <= 34.9) {
+    kommentar = "moderate Adipositas"
+}
+else if (bmi >= 35.0 && bmi <= 39.9) {
+    kommentar = "starke Adipositas"
+}
+else {
+    kommentar = "extreme Adipositas"
 }
 
-// Switch Statement 
+// Switch Statement ??
 
 console.log(kommentar);
-alert(`Du hast bei einer Größe von ${groesse}cm und einem Gewicht von ${gewicht}kg einen BMI von: ${bmi}`)
-
+auswertung.textContent = `Du hast bei einer Größe von ${groesse.toFixed(2)}m und einem Gewicht von ${gewicht}kg einen BMI von: ${bmi}`
+kommentar_auswertung.textContent = `${kommentar}`
